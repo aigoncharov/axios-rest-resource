5f9e0fb8d97b981442ff5b592d2a702da3ef3c94
keenondrums <andrey.goncharov+it@protonmail.com>
2018-07-15 05:20:41 -0600

    Add resource tests

--- src/resouce.test.ts
+++ src/resouce.test.ts
@@ -1,53 +1,163 @@
 import { AxiosInstance, AxiosRequestConfig } from 'axios'
 import { expect } from 'chai'
+import * as moxios from 'moxios'
 import { createSandbox, SinonSandbox } from 'sinon'
 
 import { AxiosResourceAdditionalProps, createAxiosInstanceFactory, createAxiosResourceFactory } from './axios'
-import { IResourceDefault, ResourceBuilder, resourceSchemaDefault } from './resource'
+import {
+  IActionMeta,
+  IAPIMethodSchema,
+  IBuildParams,
+  IBuildParamsExtended,
+  IResourceDefault,
+  ResourceBuilder,
+  resourceSchemaDefault
+} from './resource'
 
 describe('resource', () => {
   let sinon: SinonSandbox
 
   beforeEach(() => {
     sinon = createSandbox()
+    moxios.install()
   })
-  afterEach(() => sinon.restore())
+  afterEach(() => {
+    sinon.restore()
+    moxios.uninstall()
+  })
 
   describe(`${ResourceBuilder.constructor.name}`, () => {
+    const resourceBuilderValidate = async (
+      axiosConfig: AxiosRequestConfig & { baseURL: string },
+      resourceBuilderParams: IBuildParams | IBuildParamsExtended<string>,
+      resourceMethodParams: [ IActionMeta<any, any>, AxiosRequestConfig? ]
+    ) => {
+      moxios.stubRequest(/.*/, {
+        responseText: 'OK',
+        status: 200
+      })
+      const createAxiosInstance = createAxiosInstanceFactory(axiosConfig)
+      const createAxiosResource = createAxiosResourceFactory(createAxiosInstance)
+      const resourceBuilder = new ResourceBuilder(createAxiosResource)
+      const spyCreateAxiosResource = sinon.spy((resourceBuilder as any), '_createAxiosResource')
+      const resource = resourceBuilder.build(resourceBuilderParams)
+      expect(spyCreateAxiosResource.callCount).to.be.equal(1)
+      expect(typeof resource).to.be.equal('object')
+      const [ createdAxiosInstance ]: AxiosInstance[] = spyCreateAxiosResource.returnValues
+      const schema = (resourceBuilderParams as IBuildParamsExtended<string>).schema || resourceSchemaDefault
+      for (const schemaKey of Object.keys(schema)) {
+        const methodSchema = schema[schemaKey as keyof typeof schema]
+        const spyCreatedAxiosInstanceRequest = sinon.spy(createdAxiosInstance, 'request')
+        expect(resource).to.have.property(schemaKey)
+        const resourceMethod = resource[schemaKey as keyof IResourceDefault]
+        expect(typeof resourceMethod).be.equal('function')
+        const axiosPromise = resourceMethod(...resourceMethodParams)
+        expect(axiosPromise instanceof Promise).to.be.equal(true)
+        await axiosPromise
+        const requestConfigRes: AxiosRequestConfig = spyCreatedAxiosInstanceRequest.args[0][0]
+        expect(typeof requestConfigRes).to.be.equal('object')
+        expect(requestConfigRes).to.have.property('method', methodSchema.method)
+        const [ action, requestConfig ] = resourceMethodParams
+        expect(requestConfigRes).to.have.property('data', action.payload)
+        expect(requestConfigRes).to.have.property(AxiosResourceAdditionalProps, action)
+        if (requestConfig) {
+          for (const requestConfigKey of Object.keys(requestConfig)) {
+            if (requestConfigKey === 'data') {
+              continue
+            }
+            const requestConfigVal = requestConfig[requestConfigKey as keyof AxiosRequestConfig]
+            expect(requestConfigRes).to.have.property(requestConfigKey, requestConfigVal)
+          }
+        }
+        spyCreatedAxiosInstanceRequest.restore()
+      }
+      return schema
+    }
     describe('build', () => {
-      it('success: default schema', () => {
+      it('success: default schema', async () => {
         const baseURL = 'http://localhost:3000'
         const resourceURL = '/resource'
         const action = {
           payload: 'test',
           type: 'ACTION'
         }
-        const createAxiosInstance = createAxiosInstanceFactory({ baseURL })
-        const createAxiosResource = createAxiosResourceFactory(createAxiosInstance)
-        const resourceBuilder = new ResourceBuilder(createAxiosResource)
-        const spyCreateAxiosResource = sinon.spy((resourceBuilder as any), '_createAxiosResource')
-        const resource = resourceBuilder.build({
-          url: resourceURL
-        })
-        expect(spyCreateAxiosResource.callCount).to.be.equal(1)
-        expect(typeof resource).to.be.equal('object')
-        const [ createdAxiosInstance ]: AxiosInstance[] = spyCreateAxiosResource.returnValues
-        for (const defaultSchemaKey of Object.keys(resourceSchemaDefault)) {
-          const methodSchema = resourceSchemaDefault[defaultSchemaKey as keyof typeof resourceSchemaDefault]
-          const spyCreatedAxiosInstanceRequest = sinon.spy(createdAxiosInstance, 'request')
-          expect(resource).to.have.property(defaultSchemaKey)
-          const resourceMethod = resource[defaultSchemaKey as keyof IResourceDefault]
-          expect(typeof resourceMethod).be.equal('function')
-          // TODO: Mock server
-          const axiosPromise = resourceMethod(action)
-          expect(axiosPromise instanceof Promise).to.be.equal(true)
-          const requestConfig: AxiosRequestConfig = spyCreatedAxiosInstanceRequest.args[0][0]
-          expect(typeof requestConfig).to.be.equal('object')
-          expect(requestConfig).to.have.property('method', methodSchema.method)
-          expect(requestConfig).to.have.property('data', action.payload)
-          expect(requestConfig).to.have.property(AxiosResourceAdditionalProps, action)
-          spyCreatedAxiosInstanceRequest.restore()
+        const schemaRes = await resourceBuilderValidate(
+          { baseURL },
+          { url: resourceURL },
+          [ action ]
+        )
+        expect(schemaRes).to.be.equal(resourceSchemaDefault)
+      })
+
+      it('success: custom schema', async () => {
+        const baseURL = 'http://localhost:3000'
+        const resourceURL = '/resource'
+        const action = {
+          payload: 'test',
+          type: 'ACTION'
         }
+        const schema: { [ index: string ]: IAPIMethodSchema } = {
+          test: {
+            method: 'PUT'
+          }
+        }
+        const schemaRes = await resourceBuilderValidate(
+          { baseURL },
+          { url: resourceURL, schema },
+          [ action ]
+        )
+        expect(schemaRes).to.be.equal(schema)
+      })
+
+      it('success: custom request params', async () => {
+        const baseURL = 'http://localhost:3000'
+        const resourceURL = '/resource'
+        const action = {
+          payload: 'test',
+          type: 'ACTION'
+        }
+        const schema: { [ index: string ]: IAPIMethodSchema } = {
+          test: {
+            method: 'PUT'
+          }
+        }
+        const requestParams: AxiosRequestConfig = {
+          headers: {
+            test: 'test'
+          }
+        }
+        const schemaRes = await resourceBuilderValidate(
+          { baseURL },
+          { url: resourceURL, schema },
+          [ action, requestParams ]
+        )
+        expect(schemaRes).to.be.equal(schema)
+      })
+
+      it('success: custom request params with data', async () => {
+        const baseURL = 'http://localhost:3000'
+        const resourceURL = '/resource'
+        const action = {
+          payload: 'test',
+          type: 'ACTION'
+        }
+        const schema: { [ index: string ]: IAPIMethodSchema } = {
+          test: {
+            method: 'PUT'
+          }
+        }
+        const requestParams: AxiosRequestConfig = {
+          data: 'should not be overridden',
+          headers: {
+            test: 'test'
+          }
+        }
+        const schemaRes = await resourceBuilderValidate(
+          { baseURL },
+          { url: resourceURL, schema },
+          [ action, requestParams ]
+        )
+        expect(schemaRes).to.be.equal(schema)
       })
     })
   })
