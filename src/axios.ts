bfbdc07b3630baa40b2f77dc841008a5045dc365
keenondrums <andrey.goncharov+it@protonmail.com>
2018-07-17 14:48:17 +0300

    Ad src/axios.ts documentation

--- src/axios.ts
+++ src/axios.ts
@@ -11,7 +11,7 @@ export interface IAxiosResourceRequestConfig extends AxiosRequestConfig {
 
 /**
  * @description
- * Finds tokens inside of {} in config.url, matches them by keys of config.params,
+ * Axios interceptor. Finds tokens inside of {} in config.url, matches them by keys of config.params,
  * replaces them with matched values, removes matched keys from config.params
  *
  * @example
@@ -39,7 +39,7 @@ export interface IAxiosResourceRequestConfig extends AxiosRequestConfig {
  * //   params: {}
  * // }
  */
-export const interceptorUrlFormatter = (config: AxiosRequestConfig): AxiosRequestConfig => {
+const interceptorUrlFormatter = (config: AxiosRequestConfig): AxiosRequestConfig => {
   if (!config.params) {
     return config
   }
@@ -62,6 +62,33 @@ const actionHasMetaAuthorization = (action: unknown): action is IActionMetaAutho
   const actionTyped = action as IActionMetaAuthorization
   return !!(actionTyped.meta && actionTyped.meta.authorization)
 }
+/**
+ * @description
+ * Axios resource interceptor. Requires config to be IAxiosResourceRequestConfig.
+ * Finds action inside of config by AxiosResourceAdditionalProps, if action.meta has authorization,
+ * adds Authorization header = action.meta.authorization. Useful for JWT tokens.
+ *
+ * @example
+ * const axiosInstance = axios.create()
+ * axiosInstance.interceptors.request.use(interceptorAuthorizationToken)
+ * axiosInstance.request({
+ *   url: '/',
+ *   baseUrl: 'http://localhost:3000/resource',
+ *   method: 'POST',
+ *   [AxiosResourceAdditionalProps]: {
+ *     action: { meta: 'testToken', type: 'ACTION' }
+ *   }
+ * })
+ * // interceptorAuthorizationToken processes config before making an ajax request. Processed config:
+ * // {
+ * //   url: '/',
+ * //   baseUrl: 'http://localhost:3000/resource',
+ * //   method: 'POST',
+ * //   headers: {
+ * //     Authorization: 'testToken'
+ * //   }
+ * // }
+ */
 export const interceptorAuthorizationToken = (config: AxiosRequestConfig) => {
   const configExtended = config as IAxiosResourceRequestConfig
   const action = configExtended[AxiosResourceAdditionalProps].action
@@ -81,6 +108,21 @@ export interface ICreateAxiosInstanceFactoryParams extends AxiosRequestConfig {
   interceptors?: IAxiosResourceInterceptor[]
 }
 export type ICreateAxiosInstanceFromUrl = (resourceUrl: string) => AxiosInstance
+/**
+ * @description
+ * Factory that accepts default axios request config and an optional array of request interceptors,
+ * returns a function that accepts a resource url and returns a configured axios instance.
+ * Always applies default interceptorUrlFormatter to allow token substituion in url. @see interceptorUrlFormatter
+ *
+ * @example
+ * const createAxiosResource = createAxiosResourceFactory({ baseUrl: 'http://localhost:3000' })
+ * const entity1Resource = createAxiosResource('/entity1') // baseUrl will be http://localhost:3000/entity1
+ * const entity2Resource = createAxiosResource('/entity2') // baseUrl will be http://localhost:3000/entity2
+ * entity1Resource.request({ url: '/', method: 'GET' })
+ * // sends GET http://localhost:3000/entity1
+ * entity2Resource.request({ url: '/{id}', method: 'DELETE', params: { id: '123' } })
+ * // sends DELTE http://localhost:3000/entity2/123
+ */
 export const createAxiosResourceFactory = (
   {
     interceptors = [],
